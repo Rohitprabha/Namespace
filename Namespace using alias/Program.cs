809066d4e8bb90477abaa9d58e06c6c2df77ffb9
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-21 18:22:27 +0530

    Add files via upload

---
+++ Namespace using alias/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PROA = Project_A.ClassA;                           //use alias for avoiding ambiguity
+using PROB = Project_B.ClassA;
+namespace Namespace_Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PROA.print();
+            PROB.print();
+        }
+    }
+}
