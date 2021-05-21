48d1075a6aa9d5d18eb00eaf6f16bfa5de1ac92b
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-21 18:33:55 +0530

    Add files via upload

---
+++ Nested Namespace/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using nested1.ProjectC;
+
+namespace nested1
+{
+    namespace ProjectC
+    {
+        class ClassA
+        {
+            public void print()
+            {
+                Console.WriteLine("Nested Namespace");
+            }
+        }
+    }
+}
+
+namespace Nested_Namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassA c = new ClassA();
+            c.print();
+        }
+    }
+}
+
+
