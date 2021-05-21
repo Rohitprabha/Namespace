36b8c43ea5ea1d4b98f8ee6db7fe53834fd7ed72
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-21 18:28:15 +0530

    Add files via upload

---
+++ Namespace using alias/Project_B/ClassA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Project_B
+{
+
+    public class ClassA
+    {
+        public static void print()
+        {
+            Console.WriteLine("This is from ProjectB ClassA");
+        }
+        static void Main(string[] args)
+        {
+            print();
+        }
+    }
+}
