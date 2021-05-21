3324c4e267caf78d651641918b7c5a340a77de57
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-21 18:07:37 +0530

    Add files via upload

---
+++ Namespace/ProjectA/ClassA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Project_A
+{
+    public class ClassA
+    {
+        public static void print()
+        {
+            Console.WriteLine("This is from ProjectA ClassA");
+        }
+        static void Main(string[] args)
+        {
+            print();
+        }
+    }
+}
