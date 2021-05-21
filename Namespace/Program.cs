eb98caf49cce089df6657f63acab83ea568d4bb1
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-21 18:04:27 +0530

    Add files via upload

---
+++ Namespace/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Project_A;
+using Project_B;
+
+namespace Namespaces_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Project_A.ClassA.print();
+            Project_B.ClassA.print();
+
+        }
+    }
+}
